fce19056e8b018de67c79d533c671131680ba824
volumio <volumio@localhost>
2019-01-10 13:08:48 +0000

    Added PID file support and ~/.castrc to store the configured Chromecast device.
    you can now invoke the script with "restart" and it will try to kill
    the running instance before restarting the agent

--- volumio2chromecast.sh
+++ volumio2chromecast.sh
@@ -6,19 +6,34 @@
 #
 # That will run the script every minute and keep the 
 # agent running
+# 
+# Run manually with "restart" as runtime arg and it will force
+# restart the app
 #
 
 AGENT=~/volumio2chromecast/volumio2chromecast.py
-
-# Customise this for your given Chromecast device
-CHROMECAST="Office Chromecast"
+PID_FILE=/tmp/cast.pid
 
 cd
-RC=`pgrep -u ${USER} -f ${AGENT} | wc -l`
+# Load config
+. ~/.castrc
+
+# Optional restart
+if [ "${1}" == "restart" ] && [ -f ${PID_FILE} ]
+then
+    echo "Killing running agent..pid:`cat ${PID_FILE}`"
+    kill `cat ${PID_FILE}`
+    sleep 2
+fi
+
+# Check if PID File value is a running process
+RC=`pgrep -F ${PID_FILE} 2>/dev/null | wc -l` 
 if [ ${RC} -eq 0 ]
 then
     echo "Starting ${AGENT} agent in background" 
+    echo "Chromecast: ${CHROMECAST}" 
     LC_ALL=en_US.UTF-8 ${AGENT} --name "${CHROMECAST}" > /dev/null 2>&1 &
+    echo $! > ${PID_FILE}
 else
     echo "${AGENT} is already running" 
 fi
