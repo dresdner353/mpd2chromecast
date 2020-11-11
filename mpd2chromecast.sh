90b0f059d61c893ddd75f6b98b579df57b6713e2
Cormac Long <dresdner353@gmail.com>
2020-11-11 19:11:34 +0000

    Rework of script to allow it be used in three modes:
    * mpd2chromecast.sh start
      starts the python script (if not already running)
    
    * mpd2chromecast.sh stop
      stops the python script (if already running)
    
    * mpd2chromecast.sh restart
      restarts the python script (if already running)
    
    * mpd2chromecast.sh keepalive
      enters a mode where it watches every 5 seconds and restarts
      it. Works better for a more agressive restart if it crashes

--- mpd2chromecast.sh
+++ mpd2chromecast.sh
@@ -11,27 +11,76 @@
 # restart the app
 #
 
+# exit on errors
+set -e
+
 AGENT=~/mpd2chromecast/mpd2chromecast.py
 PID_FILE=/tmp/cast.pid
+FLOCK_FILE=/tmp/cast.lock
 
 cd
 
-# Optional restart
-if [ "${1}" == "restart" ] && [ -f ${PID_FILE} ]
+keep_alive() {
+    # permanent loop to monitor the 
+    # script every 5 seconds, restarting if not 
+    # running
+    while true
+    do
+        sleep 5
+        RC=`pgrep -F ${PID_FILE} 2>/dev/null | wc -l` 
+        if [ ${RC} -eq 0 ]
+        then
+            echo "`date` Restarting ${AGENT} agent in background" 
+            LC_ALL=en_US.UTF-8 ${AGENT} > /dev/null 2>&1 &
+            echo $! > ${PID_FILE}
+        else
+            echo "`date` ${AGENT} is running" 
+        fi
+    done
+}
+
+# Default action
+if [ "${1}" == "" ] 
 then
-    echo "Killing running agent..pid:`cat ${PID_FILE}`"
+    action="start"
+else
+    action="${1}"
+fi
+
+if [[ "${action}" == "keepalive" ]] 
+then
+    # lock it
+    {
+        flock -n 200 || { echo keepalive script already running ; exit 1; }
+        keep_alive
+    } 200>${FLOCK_FILE}
+
+    # this will never be called
+    # but its there in case :)
+    exit
+fi
+
+# Normal stop/start/restart usage 
+# Optional restart
+if [[ "${action}" == "restart" || "${action}" == "stop" ]] && [ -f ${PID_FILE} ]
+then
+    echo "`date` Killing running agent..pid:`cat ${PID_FILE}`"
     kill `cat ${PID_FILE}`
+    rm ${PID_FILE}
     sleep 2
 fi
 
-# Check if PID File value is a running process
-RC=`pgrep -F ${PID_FILE} 2>/dev/null | wc -l` 
-if [ ${RC} -eq 0 ]
+if [[ "${action}" == "restart" || "${action}" == "start" ]] 
 then
-    echo "Starting ${AGENT} agent in background" 
-    LC_ALL=en_US.UTF-8 ${AGENT} > /dev/null 2>&1 &
-    echo $! > ${PID_FILE}
-else
-    echo "${AGENT} is already running" 
+    # Check if PID File value is a running process
+    RC=`pgrep -F ${PID_FILE} 2>/dev/null | wc -l` 
+    if [ ${RC} -eq 0 ]
+    then
+        echo "`date` Starting ${AGENT} agent in background" 
+        LC_ALL=en_US.UTF-8 ${AGENT} > /dev/null 2>&1 &
+        echo $! > ${PID_FILE}
+    else
+        echo "`date` ${AGENT} is already running" 
+    fi
 fi
 
