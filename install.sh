d4c8463a833917a88e83e54bb54813bed86f554a
Payam Nab <papampi@users.noreply.github.com>
2024-06-11 10:34:22 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,32 +4,32 @@
 
 if [ "$UID" -eq 0 ]; then
   if [ -n "$SUDO_USER" ]; then
-    MOODE_USER="$SUDO_USER"
+    HOME_USER="$SUDO_USER"
   else
-    MOODE_USER=$(id -u -n)
+    HOME_USER=$(id -u -n)
   fi
-  MOODE_HOME="/home/$MOODE_USER"
+  HOME_DIR="/home/$HOME_USER"
   echo "The script is running as root user."
 else
   echo "This script must be run as root (or with sudo)"
   exit 1
 fi
-echo "Detected User:$MOODE_USER"
+echo "Detected User:$HOME_USER"
 # install
 function install_mpd2chromecast {
   # go to home
   cd || exit
-  echo "Installing mpd2chromecast user:${MOODE_USER} pwd:${MOODE_HOME}"
+  echo "Installing mpd2chromecast user:${HOME_USER} pwd:${HOME_DIR}"
   # GIT repo
   echo "Downloading or updating mpd2chromecast..."
   # Check if mpd2chromecast folder exists
-  if [ -d "$MOODE_HOME/mpd2chromecast" ]; then
+  if [ -d "$HOME_DIR/mpd2chromecast" ]; then
     # If it exists, change to the directory and run git pull
-    cd "$MOODE_HOME/mpd2chromecast"
+    cd "$HOME_DIR/mpd2chromecast"
     git pull
   else
     # If it doesn't exist, clone the repository
-    git clone https://github.com/papampi/mpd2chromecast.git "$MOODE_HOME/mpd2chromecast"
+    git clone https://github.com/papampi/mpd2chromecast.git "$HOME_DIR/mpd2chromecast"
   fi
 
   # Purge old entries from crontab if cron is installed
@@ -37,9 +37,9 @@ function install_mpd2chromecast {
   then
     echo "purging old crontab entries"
     # filter out existing entries
-    crontab -l | sed -e '/mpd2chromecast/d' >/tmp/"${MOODE_USER}".cron
+    crontab -l | sed -e '/mpd2chromecast/d' >/tmp/"${HOME_USER}".cron
     # reapply filtered crontab
-    crontab /tmp/"${MOODE_USER}".cron
+    crontab /tmp/"${HOME_USER}".cron
   fi
 }
 
@@ -48,19 +48,19 @@ export -f install_mpd2chromecast
 
 # main()
 # install mod2chromecast
-su "$MOODE_USER" -c "bash -c install_mpd2chromecast"
+su "$HOME_USER" -c "bash -c install_mpd2chromecast"
 
 # update and install packages
 apt-get update
 apt-get -y install python3-pip git
-cd "$MOODE_HOME"/mpd2chromecast || exit
+cd "$HOME_DIR"/mpd2chromecast || exit
 pip3 install -r requirements.txt --break-system-packages
 
 # systemd service
-echo "Systemd steps for ${MOODE_HOME}/mpd2chromecast/mpd2chromecast.service"
+echo "Systemd steps for ${HOME_DIR}/mpd2chromecast/mpd2chromecast.service"
 
 # create a user-specific variant of service file
-sed -e "s/__USER__/${MOODE_USER}/g"  "${MOODE_HOME}"/mpd2chromecast/mpd2chromecast.service >/tmp/mpd2chromecast.service
+sed -e "s/__USER__/${HOME_USER}/g"  "${HOME_DIR}"/mpd2chromecast/mpd2chromecast.service >/tmp/mpd2chromecast.service
 
 # install and start service
 cp /tmp/mpd2chromecast.service /etc/systemd/system
