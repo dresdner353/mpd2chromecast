54ac7f0a2efac330884d4f144fe595d322740a67
dresdner353 <dresdner353@gmail.com>
2024-07-05 09:42:24 +0100

    rework for bookworm or later and pre-bookworm installs. Seems to be the most abstract way to sort this install out for now.
    bookworm or later will pull packages from apt only (as they want) and
    pre-bokworm will pull in git and python only and the rest via pip
    modules
    
    Should sort out both Moode (bookworm) and Volumio (pre-bookworm) for now
    including older build variations or either one

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Exit on errors
-set -e
-
 # install
 function install_mpd2chromecast {
     cd # go to home
@@ -35,9 +32,26 @@ else
     exit 1
 fi
 
-# install packages 
+# source the /etc/os-release file to 
+# collect various platform variables
+. /etc/os-release
+
+# refresh package sources
 apt-get update
-apt-get -y install git python3-cherrypy3 python3-mpd2 python3-pychromecast
+
+if [ "${VERSION_ID}" -ge "12" ]
+then
+    # install all packages from apt
+    # bookworm and presumably beyond
+    echo "Performing Bookworm or later package install (all from apt)"
+    apt-get -y install git python3-pip python3-cherrypy3 python3-mpd2 python3-pychromecast
+else
+    # everything prior to bookworm
+    # git, python/pip from apt and pip for the modules
+    echo "Performing pre-Bookworm install (pip3 for python modules)"
+    apt-get -y install git python3-pip 
+    pip3 install pychromecast cherrypy python-mpd2
+fi
 
 # install mod2chromecast
 su ${HOME_USER} -c "bash -c install_mpd2chromecast"
@@ -54,5 +68,6 @@ sed -e "s%__USER__%${HOME_USER}%g" \
 # install and start service
 mv /tmp/mpd2chromecast.service /etc/systemd/system
 systemctl daemon-reload
+echo "Starting mpd2chromecast service"
 systemctl enable mpd2chromecast
 systemctl restart mpd2chromecast
